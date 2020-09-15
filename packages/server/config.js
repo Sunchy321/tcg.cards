5070edfa458c6c92a4c58fc689cf2786e3a7d6a3
Sunchy321 <sunchy321@qq.com>
2020-09-15 19:44:41 +0800

    ignore updated

--- packages/server/config.js
+++
@@ -1,7 +0,0 @@
-export const enableControl = true;
-
-export const __data = '../data';
-
-export const asset = __data + '/asset';
-export const data = __data + '/data';
-export const log = __data + '/log';
