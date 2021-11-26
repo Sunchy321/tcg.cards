c745577f2beccea7462bfc24f70a9db75c74c97f
Sunchy321 <sunchy321@qq.com>
2021-11-26 23:41:19 +0800

    package update

---
+++ packages/server/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const EmitAllPlugin = require('webpack-emit-all-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    entry: './src/app.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        sourceMapFilename: '[name].js.map',
+        clean: true
+    },
+    target: 'node',
+    externalsPresets: { node: true },
+    externals: [nodeExternals()],
+    resolve: {
+        extensions: ['.ts', '.js'],
+        plugins: [new TsconfigPathPlugin()]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'ts-loader'
+                }]
+            },
+            {
+                test: /\.pegjs/,
+                use: [{
+                    loader: 'pegjs-loader'
+                }]
+            }
+        ]
+    }
+};
