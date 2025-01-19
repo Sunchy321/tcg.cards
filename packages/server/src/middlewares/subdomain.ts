5273d5121109dd93cdee1ae1eac245b31c790de6
Sunchy321 <sunchy321@qq.com>
2025-01-19 19:00:13 +0800

    chore(renderer): update canvas version

--- packages/server/src/middlewares/subdomain.ts
+++ packages/server/src/middlewares/subdomain.ts
@@ -1,13 +1,14 @@
 import { Middleware } from 'koa';
 
-import KoaRouter from '@koa/router';
+import KoaRouter, { RouterContext } from '@koa/router';
+import { WebSocketContext } from 'koa-easy-ws';
 
 import { last } from 'lodash';
 
 function subdomain<StateT, ContextT>(
     sub: string,
     router: KoaRouter<StateT, ContextT>,
-): Middleware<StateT, KoaRouter.RouterContext<StateT, ContextT>> {
+): Middleware<StateT, RouterContext<StateT, ContextT> & WebSocketContext<'ws'>> {
     return async function (ctx, next) {
         if (last(ctx.subdomains) === sub) {
             return router.routes()(ctx, next);
