509f0dcaf06c8e04b37a1774a22080e37d86142b
Sunchy321 <sunchy321@qq.com>
2025-04-26 09:28:38 +0800

    feat(client): set favicon

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -4,48 +4,53 @@
     </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, watch } from 'vue';
+<script setup lang="ts">
+import { watch } from 'vue';
 
 import { useRoute } from 'vue-router';
+import { useFavicon } from '@vueuse/core';
 import { useCore } from 'store/core';
 
 import { Game } from 'static/index';
 
-export default defineComponent({
-    name: 'App',
+const route = useRoute();
+const favicon = useFavicon();
+const core = useCore();
 
-    setup() {
-        const route = useRoute();
-        const core = useCore();
+watch(
+    () => route.name?.toString(),
+    name => {
+        if (name == null || name === '') {
+            core.game = null;
+        } else if (name.startsWith('setting')) {
+            const game = name.split('/')[1];
 
-        watch(
-            () => route.name?.toString(),
-            name => {
-                if (name == null || name === '') {
-                    core.game = null;
-                } else if (name.startsWith('setting')) {
-                    const game = name.split('/')[1];
+            if (core.isGame(game)) {
+                core.game = game as Game;
+            } else {
+                core.game = null;
+            }
+        } else {
+            const firstPart = name.split('/').filter(v => v !== '')[0];
 
-                    if (core.isGame(game)) {
-                        core.game = game as Game;
-                    } else {
-                        core.game = null;
-                    }
-                } else {
-                    const firstPart = name.split('/').filter(v => v !== '')[0];
-
-                    if (core.isGame(firstPart)) {
-                        core.game = firstPart;
-                    } else {
-                        core.game = null;
-                    }
-                }
-            },
-            { immediate: true },
-        );
-
-        core.boot();
+            if (core.isGame(firstPart)) {
+                core.game = firstPart;
+            } else {
+                core.game = null;
+            }
+        }
     },
-});
+    { immediate: true },
+);
+
+watch(() => core.game, game => {
+    if (game == null) {
+        favicon.value = '/logo.png';
+    } else {
+        favicon.value = `/${game}/logo.svg`;
+    }
+}, { immediate: true });
+
+core.boot();
+
 </script>
