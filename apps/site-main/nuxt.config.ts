22c3a1b468e8b296892a040571d8095f5e1eba07
Sunchy321 <sunchy321@qq.com>
2026-03-14 06:31:45 +0800

    feat: update favicon

--- apps/site-main/nuxt.config.ts
+++ apps/site-main/nuxt.config.ts
@@ -6,6 +6,14 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
+    },
+  },
+
   css: ['~/assets/css/main.css'],
 
   routeRules: {
--- packages/ui/nuxt.config.ts
+++ packages/ui/nuxt.config.ts
@@ -13,6 +13,14 @@ export default defineNuxtConfig({
     enabled: true,
   },
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
+    },
+  },
+
   css: ['~/assets/css/main.css'],
 
   alias: {
